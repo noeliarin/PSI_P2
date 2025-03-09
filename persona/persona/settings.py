eab3b92d01c859a9b7fe36a7c8a9cab1cf452e43
noeliarin <noelia.rincon@estudiante.uam.es>
2025-03-09 17:06:02 +0100

    Render neon 3

--- persona/persona/settings.py
+++ persona/persona/settings.py
@@ -141,6 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ruta donde se almacenarán los archivos estáticos
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
